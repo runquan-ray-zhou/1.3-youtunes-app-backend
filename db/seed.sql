5d1ffe9d4e63073653e9f136cbdef0db99da7aa9
runquan-ray-zhou <151119924+runquan-ray-zhou@users.noreply.github.com>
2024-08-09 20:11:08 -0400

    add artist_albums seed data

--- db/seed.sql
+++ db/seed.sql
@@ -20,3 +20,9 @@ VALUES
  (1, 1, 'Cherry Blossom', 'Empire Of The Sun', 'Cheery Blossom', '3:28', 'https://www.billboard.com/wp-content/uploads/2024/07/Empire-Of-The-Sun-cr-Melanie-Swerdan-press-2024-billboard-158.jpg?w=942&h=623&crop=1', 'JwJU042KU0s', true),
  (2, 2, 'My Kink Is Karma', 'Chappell Roan', 'The Rise and Fall of a Midwest Princess', '3:43', 'https://t2.genius.com/unsafe/504x504/https%3A%2F%2Fimages.genius.com%2F68122940ad2367abe956e216e133c528.1000x1000x1.png', 'ePsqyPMIg6I', true),
  (3, 3, 'Expresso', 'Sabrina Carpenter', 'Expresso', '2:55', 'https://t2.genius.com/unsafe/600x600/https%3A%2F%2Fimages.genius.com%2F4f504516b80989779e11a8b3d780daeb.1000x1000x1.png', 'eVli-tstM5E', true);
+
+INSERT INTO artists_albums(artist_id, album_id)
+VALUES
+(1,1),
+(2,2),
+(3,3);
